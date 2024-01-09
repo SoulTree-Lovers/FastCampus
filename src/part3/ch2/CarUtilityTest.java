d6a9f692e51e4607e6815114390748b597caad12
강승민 <rkdtmdals999@naver.com>
2024-01-09 15:02:25 +0900

    [feat] 04.Utility 클래스가 필요한 이유

---
+++ src/part3/ch2/CarUtilityTest.java
@@ -0,0 +1,27 @@
+package part3.ch2;
+
+import part3.ch2.model.CarDTO;
+import part3.ch2.model.CarUtility;
+
+public class CarUtilityTest {
+    public static void main(String[] args) {
+        // Q. car 정보를 출력하는 동작을 가진 Utility 클래스 설계
+        int carSn = 1110;
+        String carName = "Tesla";
+        int carPrice = 100000000;
+        String carOwner = "강승민";
+        int carYear = 2020;
+        String carType = "G";
+
+        CarDTO car1 = new CarDTO();
+        car1.carSn = carSn;
+        car1.carName = carName;
+        car1.carPrice = carPrice;
+        car1.carOwner = carOwner;
+        car1.carYear = carYear;
+        car1.carType = carType;
+
+        CarUtility carUtility = new CarUtility();
+        carUtility.carInfoPrint(car1);
+    }
+}
---
+++ src/part3/ch2/model/CarUtility.java
@@ -0,0 +1,12 @@
+package part3.ch2.model;
+
+public class CarUtility {
+    public void carInfoPrint(CarDTO car) {
+        System.out.println(car.carSn);
+        System.out.println(car.carName);
+        System.out.println(car.carPrice);
+        System.out.println(car.carOwner);
+        System.out.println(car.carYear);
+        System.out.println(car.carType);
+    }
+}
