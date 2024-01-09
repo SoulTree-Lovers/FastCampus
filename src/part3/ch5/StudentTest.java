099b8cda748d1b8ea44ecf7f66c47b01fe729619
강승민 <rkdtmdals999@naver.com>
2024-01-09 16:48:38 +0900

    [feat] 01. 동일한 구조(배열)와 이질적인 구조(클래스)

---
+++ src/part3/ch5/StudentTest.java
@@ -0,0 +1,26 @@
+package part3.ch5;
+
+import part3.ch5.model.Student;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        // Q. 정수 6개를 저장할 배열을 생성
+        int[] array = new int[6];
+
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 40;
+        array[3] = 80;
+        array[4] = 160;
+        array[5] = 320;
+
+        for (int i=0; i<array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+        // Q. 잘 설계된 Student 객체를 설계한 후 데이터를 저장하고 출력
+        Student s1 = new Student("강승민", "컴공", 25, "rkd@naver.com", 20240101, "010-1234-1234");
+
+        System.out.println(s1);
+    }
+}
