3eaf800622eb8afbbbcfe80980cc7b1020a138cc
강승민 <rkdtmdals999@naver.com>
2024-01-09 16:56:55 +0900

    [feat] 02. 기본배열과 객체배열의 관계 (객체배열 저장 후 출력하기)

---
+++ src/part3/ch5/StudentArrayTest.java
@@ -0,0 +1,27 @@
+package part3.ch5;
+
+import java.util.ArrayList;
+import part3.ch5.model.Student;
+
+public class StudentArrayTest {
+    public static void main(String[] args) {
+        // Q. 객체 배열을 이용하여 4명의 학생 데이터를 저장 후 출력
+        Student[] students = new Student[4];
+
+        students[0] = new Student("강승민", "컴공1", 25, "rkd@naver.com", 20240101, "010-1234-1234");
+        students[1] = new Student("김승민", "컴공2", 24, "rkd@naver.com", 20240101, "010-1234-1234");
+        students[2] = new Student("박승민", "컴공3", 23, "rkd@naver.com", 20240101, "010-1234-1234");
+        students[3] = new Student("이승민", "컴공4", 22, "rkd@naver.com", 20240101, "010-1234-1234");
+
+
+        for (int i=0; i< students.length; i++) {
+            System.out.println(students[i]);
+        }
+
+        System.out.println();
+
+        for (Student student: students) {
+            System.out.println(student);
+        }
+    }
+}
