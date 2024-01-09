85caf6785fe5a44a424a640df57e2f96c9819a4e
강승민 <rkdtmdals999@naver.com>
2024-01-09 15:47:13 +0900

    [feat] 02. 클래스를 접근하는 이름 이해

---
+++ src/part3/ch3/PackageNameTest.java
@@ -0,0 +1,22 @@
+package part3.ch3;
+
+// import java.lang.* -> 기본적으로 자바에서 제공하는 패키지
+import java.util.Scanner;
+// import part3.ch2.model.CarDAO;
+import part3.ch2.model.CarDTO;
+import part3.ch3.model.MemberDTO;
+
+public class PackageNameTest {
+    public static void main(String[] args) {
+        // Q. 지금까지 자바에서 제공한 클래스의 이름을 적어보자.
+        String str;
+        Scanner scanner;
+        System.out.println("hello world!");
+
+        // Q. 우리가 직접 만든 클래스 이름을 적어보자.
+        CarDTO carDTO = new CarDTO(); // 클래스 이름만 적는 방식
+        part3.ch2.model.CarDAO carDAO = new part3.ch2.model.CarDAO(); // 클래스 경로까지 포함하여 적는 방식
+        MemberDTO memberDTO = new MemberDTO();
+
+    }
+}
