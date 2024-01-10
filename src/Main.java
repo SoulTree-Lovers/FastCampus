e63a76ceafff0425a37dc115ee4e49e214ee734a
강승민 <rkdtmdals999@naver.com>
2024-01-10 16:33:05 +0900

    [feat] Main class 수정

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,8 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        /**
+         * "이 프로젝트는 패스트캠퍼스의 [시그니처 백엔드 Path 초격차 패키지 Online.] Course 1 강의를 수강하며 실습한 코드입니다. "
+        */
     }
 }
