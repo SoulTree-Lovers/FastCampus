c515219dfb4edfda34d4ab1953104a32854e4d97
강승민 <rkdtmdals999@naver.com>
2024-01-14 17:27:05 +0900

    [feat] 02. 제네릭 사용하기

---
+++ src/course2/part2/ch2/ArrayListGeneric.java
@@ -0,0 +1,19 @@
+package course2.part2.ch2;
+
+import course2.part1.ch2.model.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListGeneric {
+    public static void main(String[] args) {
+        List<Book> list = new ArrayList<>();
+
+        // 제네릭을 사용하지 않으면 Object[] 타입의 배열이 생성되므로, 여러가지 타입을 넣을 수 있다. --> 타입 안정성 x
+        list.add(new Book("java", 15000, "한빛", "홍길동"));
+        // list.add("Hello"); --> 제네릭 사용 시 에러
+        list.add(new Book("python", 15000, "한빛", "홍길동"));
+
+        System.out.println(list.get(0));
+        System.out.println(list.get(1));
+    }
+}
